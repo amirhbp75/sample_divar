35a849df24e09dd150382c404fae6956a54dbf5e
amirhbp <amirhbp75@gmail.com>
2023-11-25 19:32:30 +0330

    Implementation of the splash screen

--- app/src/main/java/app/divarinterview/android/MainActivity.kt
+++ app/src/main/java/app/divarinterview/android/MainActivity.kt
@@ -1,11 +1,22 @@
 package app.divarinterview.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+
+    private var keep = true
+    private val delay = 1000L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
         setContentView(R.layout.activity_main)
+
+        splashScreen.setKeepOnScreenCondition { keep }
+        Handler(Looper.getMainLooper()).postDelayed({ keep = false }, delay)
     }
 }
